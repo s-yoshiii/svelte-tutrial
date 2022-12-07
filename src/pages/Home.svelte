5c3214ad786c7629e71926c03ea383cdc9d3709a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-07 09:53:29 +0900

    propsの受け渡し

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,7 +1,9 @@
 <script>
-    import {link} from 'svelte-spa-router'
+    import {link} from 'svelte-spa-router';
+    import Button from '../component/Button.svelte';
     let pageName = "Home";
 </script>
 
 <h1>{pageName} page.</h1>
+<Button buttonText={"リンク"} />
 <a use:link={{href:"/about"}}>About</a>
