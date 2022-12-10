f09db65a4c1a7dd152da514a4d32b81e80270d1a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-10 22:00:15 +0900

    API取得テスト

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,8 +2,24 @@
     import {link} from 'svelte-spa-router';
     import Button from '../component/Button.svelte';
     let pageName = "Home";
+    let promise;
+    const getConirmedNumber = async() => {
+        try{
+            const res = await fetch('https://api.covid19api.com/dayone/country/japan/status/confirmed');
+            const json = await res.json();
+            const latest = await json[json.length - 1];
+            const cases = await latest.Cases;
+            console.log(cases)
+        } catch(err){
+            console.log(err);
+        }
+    }
+   function handleClick(){
+    promise = getConirmedNumber();
+   }
 </script>
 
 <h1>{pageName} page.</h1>
-<Button buttonText={"リンク"} />
+<Button buttonText={"リンク"} on:click={handleClick} />
+
 <a use:link={{href:"/about"}}>About</a>
