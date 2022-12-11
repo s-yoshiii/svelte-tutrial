adf7bf3040bdb3765c88df890154be623dad3036
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-11 16:37:19 +0900

    await blocksテスト

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,24 +2,34 @@
     import {link} from 'svelte-spa-router';
     import Button from '../component/Button.svelte';
     let pageName = "Home";
-    let promise;
     const getConirmedNumber = async() => {
         try{
             const res = await fetch('https://api.covid19api.com/dayone/country/japan/status/confirmed');
             const json = await res.json();
             const latest = await json[json.length - 1];
             const cases = await latest.Cases;
-            console.log(cases)
+            return cases;
         } catch(err){
             console.log(err);
+            throw new Error(err);
         }
     }
+   let promise = getConirmedNumber();
    function handleClick(){
     promise = getConirmedNumber();
    }
 </script>
 
 <h1>{pageName} page.</h1>
-<Button buttonText={"リンク"} on:click={handleClick} />
+<Button buttonText={"API取得"} on:click={handleClick} />
 
-<a use:link={{href:"/about"}}>About</a>
+{#await promise}
+<p>...waiting</p>
+{:then number}
+<p>The number is {number}</p>
+{:catch error}
+<p>The error is {error}</p>
+{/await}
+<div>
+    <a use:link={{href:"/about"}}>About</a>
+</div>
