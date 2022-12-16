080f2394dc910b154447b6960e857932070d5509
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-17 00:19:03 +0900

    Pico CSS Flamework

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import Router from "svelte-spa-router";
   import { routes } from "./routes";
+  import "@picocss/pico";
 </script>
 
 <main>
-  <Router {routes}/>
+  <Router {routes} />
 </main>
 
 <style>
